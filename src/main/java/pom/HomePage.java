922f4f5d8c6f27a2d5653eeb8f20b01aee526a01
PallaviNPustake <pallavinpustake@gmail.com>
2023-08-10 16:27:47 +0530

    [PallaviP] I have added homepage class.

---
+++ src/main/java/pom/HomePage.java
@@ -0,0 +1,13 @@
+package pom;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class HomePage {
+
+	@Test
+	public void Homepage()
+	{
+		Reporter.log("HomePage", true);
+	}
+}
