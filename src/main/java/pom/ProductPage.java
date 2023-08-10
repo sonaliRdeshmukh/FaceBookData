22a227825b8393da8c03f6676b2ad6849a9f15e7
sonaliRdeshmukh <deshmukhsonali2602@gmail.com>
2023-08-10 16:34:59 +0530

    [Sonali] Add new pdp page.

---
+++ src/main/java/pom/ProductPage.java
@@ -0,0 +1,14 @@
+package pom;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ProductPage 
+
+{
+    @Test
+    public void pdppage()
+    {
+    	Reporter.log("This is pdp page", true);
+    }
+}
